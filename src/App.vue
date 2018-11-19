987e11523c071634f196430695239226d24e2e8c
Luis Hernandez <managerfashion.design.mx@gmail.com>
2018-11-18 20:59:29 -0600

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <!-- <img src="./assets/logo.png"> -->
+      <h2 class="text-center">Infosheet</h2>
+      <div class="row justify-content-center">
+        <div class="col-12">Testing
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  created(){
+    axios.get('https://website.managerfashion.net/api/emsproducoes/complement/getLanguages/'+this.lang+'/null').then(response=>{
+      this.$set(this.optionList, 'languages', response.data)
+    })
+    axios.get('https://website.managerfashion.net/api/emsproducoes/complement/getSkills/'+this.lang+'/null').then(response=>{
+      this.$set(this.optionList, 'skills', response.data)
+    })
+    axios.get('https://website.managerfashion.net/api/emsproducoes/cities').then(response=>{
+      this.$set(this.optionList, 'cities', response.data)
+    })
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      lang:'en',
+      optionList:{}
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import VeeValidate from 'vee-validate';
 import BootstrapVue from 'bootstrap-vue'
 import 'font-awesome/scss/font-awesome.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+
+window.axios = axios
 Vue.use(BootstrapVue);
 Vue.use(VeeValidate);
 new Vue({
